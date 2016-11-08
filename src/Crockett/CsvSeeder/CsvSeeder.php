a875e5e801ad4b933dc938613eea400b01cdb208
Fabio Silva <faacsilva@yahoo.com>
2016-11-08 12:05:36 -0200

    Corrects the operation of the 'insert_callback' property that receives only the value passed through the method parameters

--- src/Crockett/CsvSeeder/CsvSeeder.php
+++ src/Crockett/CsvSeeder/CsvSeeder.php
@@ -208,7 +208,7 @@ class CsvSeeder extends Seeder
         $this->delimiter       = $delimiter ?: $this->delimiter;
         $this->aliases         = $aliases ?: $this->aliases;
         $this->mapping         = $mapping ?: $this->mapping;
-        $this->insert_callback = $insert_callback;
+        $this->insert_callback = $insert_callback ?: $this->insert_callback ; 
 
         $this->runSeeder();
     }
